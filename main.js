a79d82a1a093de1aeaa2af7c7d40c77bfd6c03ff
PT. Laksana Digital Industri <pt.ldi@protonmail.com>
2026-03-01 16:29:51 +0800

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Current Date and Time in UTC
-const currentDateTime = () => {
-    const now = new Date();
-    return now.toISOString().replace('T', ' ').substring(0, 19);
-};
-
-console.log(`Current Date and Time (UTC): ${currentDateTime()}`);
